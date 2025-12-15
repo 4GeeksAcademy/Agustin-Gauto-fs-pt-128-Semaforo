03c48b26c6168496434435862f45af04592e8233
AgustinMGauto <agustin_gauto@outlook.com>
2025-12-15 20:39:37 +0000

    semaforo funcionando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,20 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [luzPrendida, SetLuzprendida] = useState("")
+	return (
+		<>
+		<div className="palo"></div>
+		<div className="text-center FDS">
+			<div className="Botones">
+			<button  type="button" onClick={()=> SetLuzprendida("btn-danger")} className={`rounded-circle btn btn-danger ${luzPrendida == "btn-danger" ? "iluminacion" : " "} `}></button>
+			<button  type="button" onClick={()=> SetLuzprendida("btn-warning")} className={`rounded-circle btn btn-warning ${luzPrendida == "btn-warning" ? "iluminacion" : " "} `}></button>
+			<button  type="button" onClick={()=> SetLuzprendida("btn-success")} className={`rounded-circle btn btn-success ${luzPrendida == "btn-success" ? "iluminacion" : " "} `}></button>
+			</div>
 		</div>
+	</>
 	);
 };
 
